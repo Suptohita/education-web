374ca69fa75a32f53bcfde5ad692d9dae4d2bb9b
Arvind <arbindudebnath59985@gmail.com>
2021-10-04 18:44:54 +0600

    update About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Footer from '../Home/Footer/Footer';
+import Header from '../Home/Header/Header';
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <Header></Header>
+            <h1 className='text-center mt-3 mb-5'><u>About us</u></h1>
+            <div className='container text-wrap'>
+                <p>We help organizations of all types and sizes prepare for the path ahead — wherever it leads. Our curated collection of business and technical courses help companies, governments, and nonprofits go further by placing learning at the center of their strategies.We’re committed to changing the future of learning for the better. Dig into our original research to learn about the forces that are shaping the modern workplace.
+                    <br />
+                    <br />
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium soluta optio. Repudiandae voluptatum totam rerum suscipit, perferendis necessitatibus provident eveniet reiciendis eaque in voluptates, nisi, a iusto laboriosam? Saepe asperiores quas earum aperiam, obcaecati, provident temporibus a quibusdam minus pariatur cum natus praesentium dolorem eum reiciendis impedit optio incidunt placeat hic accusamus aliquid qui deserunt nostrum porro. Reprehenderit facere maxime eaque, tenetur rem ducimus laborum quasi numquam autem optio explicabo soluta perspiciatis! Quidem quod, omnis unde officia cupiditate at in ipsum dolor, voluptates ex ea voluptatibus veniam nemo. Quos quis iure assumenda dicta quo eum perspiciatis consectetur at praesentium quae. Hic, ratione itaque sapiente non, quos ipsum libero, suscipit aut officiis magni dicta cupiditate ut quaerat consequatur! Repellendus officiis consequuntur, accusantium non laudantium ea totam quibusdam praesentium necessitatibus sint eos temporibus sapiente dignissimos fugiat nam optio magni aliquid commodi beatae amet sit tempore neque nesciunt? Recusandae tenetur asperiores suscipit, perferendis, mollitia quas sed eligendi.
+                    <br />
+                    <br />
+                    sapiente autem totam, a officiis. Ducimus aliquid cupiditate, laborum assumenda officia velit in, numquam consequatur earum provident rerum amet sapiente error! Magni ipsum ullam consequatur laboriosam odio repellendus nemo, impedit cupiditate doloribus tempore ea beatae sint maxime ut temporibus quo, mollitia unde odit ad, enim expedita non. Necessitatibus unde doloribus dolor vero qui eum eveniet quo et impedit placeat eaque tenetur, facilis, nulla a, iure illo omnis? Obcaecati excepturi, culpa fugiat corrupti rerum nam aliquid eveniet minima, consequuntur hic unde dolorum! Cum ratione dicta facilis itaque quo pariatur commodi est saepe quaerat fugiat temporibus, alias perspiciatis assumenda vero enim reiciendis adipisci error iure voluptatibus nihil excepturi velit totam voluptatum. Rerum nostrum magnam eligendi non vel? Quos laborum, excepturi voluptatibus dolorem quas quidem illum voluptas ipsa. Facilis nobis iste expedita commodi blanditiis impedit corrupti, consequuntur pariatur ipsa repudiandae aperiam laboriosam sint quos at, animi sequi. Deserunt earum enim, modi provident nobis perspiciatis quaerat sapiente recusandae delectus sint mollitia illo esse inventore sit possimus unde corporis, labore dolores quidem accusamus at quis dolorem vero cumque! Magnam velit, architecto fuga quibusdam harum quia totam quo cum, vitae, illum exercitationem. Laborum non, animi perspiciatis nobis maiores atque soluta asperiores quas voluptatem voluptatibus provident? Fugit consequuntur illo placeat? Quaerat soluta, nisi possimus architecto recusandae facere facilis, vel consequuntur cumque optio accusamus! Nobis expedita, quos veniam eaque animi iste officiis esse cum error molestiae. Odio hic nihil beatae et quos labore distinctio deleniti unde cumque impedit cum sint consequuntur voluptatum similique praesentium, mollitia assumenda sunt fuga iste minus quae fugiat voluptates molestiae atque. Doloremque numquam molestias modi totam dolorem hic illo officia, omnis quae temporibus vel itaque fugiat obcaecati iste labore voluptate eum veritatis odio consequuntur cumque laudantium nostrum sit. Culpa aliquid magnam modi alias, molestias odio labore illo a deserunt cum vitae praesentium sunt est numquam dolores similique corporis saepe dignissimos, distinctio quod. Ipsam assumenda dolorem voluptatibus cum repudiandae? Alias, impedit dolores eveniet quisquam dicta, libero modi iure minima placeat eum tempora minus, dolorem labore consequatur corporis. Facere ea quaerat quia vel dolorem corrupti, dolore provident sunt voluptates ipsam earum!
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <h3 className='text-center'>
+                        Thank You!
+                        Thank you so much for reading this web site and being a part of our community!
+                        We hope you enjoy our tutorials and we look forward to getting to know you.
+                    </h3>
+                </p>
+                <h1 className='text-center mt-5 text-warning border'>Be With US</h1>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
